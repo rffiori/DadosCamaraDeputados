173ff975941610a0828303045a8753bb17f45858
Rodrigo Fiori <rodrigo.ffiori@gmail.com>
2018-09-02 18:14:34 -0300

    App structure and config files

---
+++ src/templates/Main/Main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Main.css';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="Main">
+        <header className="Main-header">
+          <h1 className="Main-title">Informações sobre deputados</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Main;
